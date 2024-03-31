827b6234f87289a4483ad63f7bbc5bf297fc67a2
rgtexa <dcscott@gmail.com>
2024-03-31 13:05:22 -0400

    add graceful shutdown for server

---
+++ cmd/api/server.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func (app *application) serve() error {
+	srv := &http.Server{
+		Addr:              fmt.Sprintf(":%d", app.config.port),
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+		ErrorLog:          slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
+	}
+
+	shutdownError := make(chan error)
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		s := <-quit
+
+		app.logger.Info(fmt.Sprintf("shutting down %s server", app.config.env), slog.String("signal", s.String()))
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		shutdownError <- srv.Shutdown(ctx)
+	}()
+
+	app.logger.Info(fmt.Sprintf("starting %s server on %s", app.config.env, srv.Addr))
+
+	err := srv.ListenAndServe()
+
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+
+	app.logger.Info(fmt.Sprintf("%s server stopped", app.config.env))
+
+	return nil
+}
